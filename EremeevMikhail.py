4c7e2d95a3d1de5c4b4887d733b697149e758f47
Михаил Еремеев <michaileremeev2005@gmail.comi>
2026-03-16 12:43:14 +0300

    Добавлено дополнительное округление числа Пи

--- EremeevMikhail.py
+++ EremeevMikhail.py
@@ -2,9 +2,9 @@ import math
 
 radius = 7
 area = round(math.pi) * radius ** 2 # Первый коммит в свою ветку, округление числа Пи
-circumference = 2 * math.pi * radius
+circumference = 2 * round(math.pi) * radius # Второй коммит в свою ветку, дополнительное округление числа Пи
 
-print(f"Радиус круга: {radius}")
+print(f"Радиус окружности: {radius}")
 print(f"Площадь круга: {round(area, 2)}")
 print(f"Длина окружности: {round(circumference, 2)}")
 print(f"Проверка: {radius}² * π ≈ {round(radius**2 * 3.14, 2)}")
