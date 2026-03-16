9fa20d7c5a052cf498d638b09aeaf581bbd0fc89
Михаил Еремеев <michaileremeev2005@gmail.comi>
2026-03-16 12:29:24 +0300

    Добавлен вывод числа Пи

--- EremeevMikhail.py
+++ EremeevMikhail.py
@@ -8,3 +8,5 @@ print(f"Радиус круга: {radius}")
 print(f"Площадь круга: {round(area, 2)}")
 print(f"Длина окружности: {round(circumference, 2)}")
 print(f"Проверка: {radius}² * π ≈ {round(radius**2 * 3.14, 2)}")
+
+print(f"Число Пи: {math.pi}") # Первый коммит в мастера, вывод числа пи
